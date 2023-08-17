f7018884e2ef3b4a2c0b22c5688920e6a90a8efe
Marcus.W <marcus.w@laggelee.se>
2023-08-17 09:59:06 +0200

    change position of the Navbar according to wireframes, add the text previously included in the Image

--- ui/Home/Home.js
+++ ui/Home/Home.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { ImageBackground, StyleSheet, View } from "react-native";
+import { ImageBackground, StyleSheet, View, Text } from "react-native";
 import List from "./HomeList";
 import Navbar from "../Navbar";
 
 const Home = () => {
   return (
     <View style={styles.container}>
+      <View style={styles.navbarContainer}>
+        <Navbar showArrow={false} />
+      </View>
       <ImageBackground
         style={styles.heroImage}
         source={require("../../assets/hero.png")}
       >
-        <View style={styles.navbarContainer}>
-          <Navbar showArrow={false} />
-        </View>
+        <Text style={styles.heroText}>Our Gyms</Text>
       </ImageBackground>
       <List />
     </View>
@@ -29,10 +30,31 @@ const styles = StyleSheet.create({
   heroImage: {
     width: 393,
     height: 400,
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+    flexShrink: 0,
+    gap: 8,
+    paddingVertical: 40,
     paddingHorizontal: 24,
   },
   navbarContainer: {
-    marginTop: 80,
+    position: "absolute",
+    top: 80,
+    zIndex: 1,
+    left: 0,
+    right: 0,
+    zIndex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  heroText: {
+    color: "#FFFFFF",
+    fontFamily: "WorkSans",
+    fontSize: 56,
+    fontStyle: "normal",
+    fontWeight: 700,
+    lineHeight: 61.6,
   },
 });
 
